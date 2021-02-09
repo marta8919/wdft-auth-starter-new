ca2d459ff9fee5fce09f589b2473abdedbbca033
Marta <marta.gilabert.guzman@gmail.com>
2021-02-09 15:41:19 +0100

    activity

--- app.js
+++ app.js
@@ -24,16 +24,36 @@ const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowe
 
 app.locals.title = `${capitalized(projectName)}- Generated with IronGenerator`;
 
+// Set up connect-mongo
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose')
+
+app.use(session({
+    secret: 'NotMyAge',
+    saveUninitialized: false, 
+    resave: false, 
+    cookie: {
+      maxAge: 1000*60*60*24// is in milliseconds.  expiring in 1 day
+    },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60*60*24, // is in seconds. expiring in 1 day
+    })
+}));
+
+
 // 👇 Start handling routes here
 const index = require("./routes/index");
 app.use("/", index);
 
 //link your auth routes here
-const authRoutes = require("./routes/auth.routes")
-app.use("/", authRoutes)
+const authRoutes = require("./routes/auth.routes");
+app.use("/", authRoutes);
 
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;
+
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,38 +1,127 @@
-//create your routes here
 const router = require("express").Router();
 const bcrypt = require('bcryptjs');
 const UserModel = require('../models/User.model')
 
-// 1. Add a GET route for /signup . 
-// Show the signup.hbs file when the user visits that route
-router.get('/signup', (req, res, next)=>{
-  res.render('./auth/signup')
+/* GET signin page */
+router.get("/signin", (req, res, next) => {
+    // Shows the sign in form to the user
+    res.render('auth/signin.hbs')
+});
+
+/* GET signup page */
+router.get("/signup", (req, res, next) => {
+    // Shows the sign up form to the user
+    res.render('auth/signup.hbs')
+});
+
+// Handle POST requests to /signup
+// when the user submits the data in the sign up form, it will come here
+router.post("/signup", (req, res, next) => {
+    // we use req.body to grab data from the input form
+     const {name, email, password} = req.body
+    //console.log(req.body) // check if this is an empty object
+    // if not use the length 
+
+
+    //validate first
+    // checking if the user has entered all three fields
+    // we're missing one important step here
+    if (!name.length || !email.length || !password.length) {
+        res.render('auth/signup', {msg: 'Please enter all fields'})
+        return;
+    }
+
+    // validate if the user has entered email in the right format ( @ , .)
+     // regex that validates an email in javascript
+     let re = /\S+@\S+\.\S+/;
+     if (!re.test(email)) {
+        res.render('auth/signup', {msg: 'Email not in valid format'})
+        return;
+     }
+
+     //validate password (special character, some numbers, min 6 length)
+     /*
+     let regexPass = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/;
+     if (!regexPass.test(password)) {
+        res.render('auth/signup', {msg: 'Password needs to have special chanracters, some numbers and be 6 characters aatleast'})
+        return;
+     }
+
+     */
+
+     // NOTE: We have used the Sync methods here. 
+     // creating a salt 
+     let salt = bcrypt.genSaltSync(10);
+     let hash = bcrypt.hashSync(password, salt);
+     UserModel.create({name, email, password: hash})
+        .then(() => {
+            res.redirect('/')
+        })
+        .catch((err) => {
+            next(err)
+        })
+});
+
+// handle post requests when the user submits something in the sign in form
+router.post("/signin", (req, res, next) => {
+    const {email, password} = req.body
+
+    // implement the same set of validations as you did in signup as well
+    // NOTE: We have used the Async method here. Its just to show how it works
+    UserModel.findOne({email: email})
+        .then((result) => {
+            // if user exists
+            if (result) {
+                //check if the entered password matches with that in the DB
+                bcrypt.compare(password, result.password)
+                    .then((isMatching) => {
+                        if (isMatching) {
+                            // when the user successfully signs up
+                            req.session.loggedInUser = result
+                            res.redirect('/profile')
+                        }
+                        else {
+                            // when passwords don't match
+                            res.render('auth/signin.hbs', {msg: 'Passwords dont match'})
+                        }
+                    })
+            }
+            else {
+                // when the user signs in with an email that does not exits
+                res.render('auth/signin.hbs', {msg: 'Email does not exist'})
+            }
+        })
+        .catch((err) => {
+            next(err)
+        })
+   
+});
+
+
+// GET request to handle /profile
+
+
+const checkLoggedInUser = (req, res, next) => {
+  if (req.session.loggedInUser) {
+      next()
+  }
+  else {
+      res.redirect('/signin')
+  }
+}
+
+
+router.get('/profile',checkLoggedInUser, (req, res, next) => {
+    let email = req.session.loggedInUser.email
+    res.render('profile.hbs', {email})
 })
 
-// 2. Add a GET route for /signin . 
-// Show the signin.hbs file when the user visits that route
-router.get('/signin', (req, res, next)=>{
-  res.render('./auth/signin')
+
+router.get('/logout', (req, res) => {
+  req.session.destroy()
+  res.redirect('/')
 })
 
-router.post("/signup", (req, res, next)=>{
-  const {name, email, password} = req.body
-    // creating a salt 
-    let salt = bcrypt.genSaltSync(10);
-    let hash = bcrypt.hashSync(password, salt);
-      UserModel.create({name, email, password: hash})
-      .then(() => {
-        res.redirect('/')
-      })
-      .catch((err) => {
-          next(err)
-      })
-  
-})
 
-// 3. don't forget to export your router with 'modeul.exports'
 
-module.exports = router
-
-// And finally don't forget to link this router in your middleware at the bottom of app.js where the other routes are defined
-
+module.exports = router;
